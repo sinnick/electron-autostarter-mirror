cbf8b7459b4392e3f9958e79c78f4687daddf23e
Fernando Massó <fercsbmwlp@gmail.com>
2025-01-23 19:24:19 -0300

    starter

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+require('electron-reload')(__dirname, {
+	electron: require(`${__dirname}/node_modules/electron`)
+});
+
+function createWindow() {
+	const mainWindow = new BrowserWindow({
+		fullscreen: true,
+		autoHideMenuBar: true,
+		frame: false, // Oculta la barra de título
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	mainWindow.loadURL('http://localhost:3000');
+	// mainWindow.loadURL('https://apps.visionblo.com');
+	mainWindow.setFullScreenable(true);
+	mainWindow.setFullScreen(true); // Asegura que la ventana esté en pantalla completa
+	mainWindow.setMenu(null); // Deshabilita el menú
+}
+
+app.whenReady().then(createWindow);
